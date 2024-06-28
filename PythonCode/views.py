033be3a0d3b0f4ffcba308941832a4143f231d5a
kayamvamsikrishna <kayamvamsikrishna@gmail.com>
2024-06-28 21:14:21 +0530

    first commit

--- PythonCode/views.py
+++ PythonCode/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
+def isDisariumNumber(a):
+    dummy=a
+    summ=0
+    l=len(str(a))
+    while dummy>0:
+        rem=dummy%10
+        dummy//=10
+        summ+=rem**l
+        l-=1
+    if summ==a:
+        return True
+    else:
+        return False
+if __name__=='__main__':
+    ll=int(input('Enter lower limit='))
+    ul=int(input('Enter upper limit='))
+    for a in range(ll,ul+1):
+        if isDisariumNumber(a):
+            print(a,' ',end='')
